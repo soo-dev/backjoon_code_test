fce6d467e678c89fbd0aba58db5a36660a2e172c
soo <sooyoungs0120@gmail.com>
2022-04-11 16:58:04 +0900

    Stage 1 - input, output, arithmetic operation

--- Main_10869.java
+++ Main_10869.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
-public class Main_1000 {
+public class Main_10869 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		
-		System.out.println(a + b);
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
 	}
 }
---
+++ Main_2588.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main_2588 {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int units = b%10 * a;
+		int tens = b%100/10 * a;
+		int hundreds = b%1000/100 * a;
+		int sum = a * b;
+		System.out.println(units);
+		System.out.println(tens);
+		System.out.println(hundreds);
+		System.out.println(sum);
+	}
+}
